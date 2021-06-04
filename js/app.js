7b7725e3cf0fbaee1e62fbde553b7ba42f3889bc
ju0910 <leejooeun0910@gmail.com>
2021-06-04 17:25:56 +0900

    added more videos

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 const video1 = document.querySelector('#video1')
 const video2 = document.querySelector('#video2')
+const video3 = document.querySelector('#video3')
+const video4 = document.querySelector('#video4')
 const start = document.querySelector('#start')
 const change = document.querySelector('#change')
 
@@ -10,15 +12,19 @@ video1.addEventListener('canplaythrough', () => {
 start.addEventListener('click', () => {
     video1.muted = false
     //video1.volume = 0.1
-    video1.currentTime = 10.366666
+    video1.currentTime = 10.8
     video1.style.opacity = 1
     video1.play()
-    video2.currentTime = 1.166666
+    video2.currentTime = 10.366666
     video2.play()
+    video3.currentTime = 1.166666
+    video3.play()
+    video4.currentTime = 0.333333
+    video4.play()
 })
 
 change.addEventListener('click', () => {
-    video2.style.opacity = video2.muted ? 1 : 0  
-    video2.muted = video2.muted ? false : true    
+    video4.style.opacity = video4.muted ? 1 : 0  
+    video4.muted = video4.muted ? false : true    
     video1.muted = video1.muted ? false : true
 })
