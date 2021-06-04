afa8dacdcb359ed374e83c067804ff7070b9865f
ju0910 <leejooeun0910@gmail.com>
2021-06-04 18:27:51 +0900

    synced videos

--- js/app.js
+++ js/app.js
@@ -1,7 +1,4 @@
-const video1 = document.querySelector('#video1')
-const video2 = document.querySelector('#video2')
-const video3 = document.querySelector('#video3')
-const video4 = document.querySelector('#video4')
+const videos = document.querySelectorAll('video')
 const start = document.querySelector('#start')
 const change = document.querySelector('#change')
 
@@ -12,15 +9,12 @@ video1.addEventListener('canplaythrough', () => {
 start.addEventListener('click', () => {
     video1.muted = false
     //video1.volume = 0.1
-    video1.currentTime = 10.8
     video1.style.opacity = 1
-    video1.play()
-    video2.currentTime = 10.366666
-    video2.play()
-    video3.currentTime = 1.144444
-    video3.play()
-    video4.currentTime = 0.266666
-    video4.play()
+
+    for (const video of videos) {
+        video.currentTime = video.dataset.starttime
+        video.play()
+    }
 })
 
 change.addEventListener('click', () => {
