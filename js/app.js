c117f2285600e0925037e3dcab897bb796877f7f
Jiseung Jung <roadrunner23@yonsei.ac.kr>
2021-06-18 16:19:06 +0900

    organize code

--- js/app.js
+++ js/app.js
@@ -1,31 +1,27 @@
-const videos = Array.from (document.querySelectorAll('video'))
+const videos = Array.from(document.querySelectorAll('video'))
 const videoContainer = document.querySelector('.video-container')
 const start = document.querySelector('#start')
 const change = document.querySelector('#change')
 let currentVideo = video1
 
-video1.addEventListener('canplaythrough', () => {
-    //video1.style.opacity = 1
-})
-
 start.addEventListener('click', () => {
     video1.muted = false
-    //video1.volume = 0.1
 
     for (const video of videos) {
         video.currentTime = video.dataset.starttime
+        video.volume = 0.1
         video.play()
-    }    
+    }
 })
 
-//!==
-for (let idx in videos){
-const video = videos[idx]
-video.addEventListener('click', () => {
-    const prevVideo = document.querySelector('video.selected')
-    prevVideo.classList.remove('selected')
-    video.classList.add('selected')
-    videoContainer.style.transform = `translateX(${idx * -25 + 25}%)`
-})
-
+for (let idx in videos) {
+    const video = videos[idx]
+    video.addEventListener('click', () => {
+        const prevVideo = document.querySelector('video.selected')
+        prevVideo.classList.remove('selected')
+        prevVideo.muted = true
+        video.classList.add('selected')
+        video.muted = false
+        videoContainer.style.transform = `translateX(${idx * -25 + 25}%)`
+    })
 }
